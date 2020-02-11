80857e0f907d25c528a624a89582456cf2be0331
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-11 09:55:47 -0500

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
+* _islower - Return 1 if letter is lowercase.
+* @c: char character.
+* Return: 1 if lowercase, 0 if not lowercase
 */
-int _islower(int c){
+int _islower(int c)
+{
 if (c >= 97 && c <= 122)
 {
 return (1);
 }
 else
 {
-return(0);
+return (0);
 }
 }
