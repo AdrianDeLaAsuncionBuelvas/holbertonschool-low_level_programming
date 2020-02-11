5c429958b5fdcd91db778695e6acc4d7d77a1f56
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-11 16:26:22 -0500

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* add - adds two integers and returns the result.
+*@a: int type Integer.
+*@b: int type Integer.
+* Return: a, b.
+*/
+
+int add(int a, int b)
+{
+return (a + b);
+}
