c8878d8f2f81478f31b13d65471ab5ebbd6cc1f2
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-11 10:06:31 -0500

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
-
+/**
+* print_sign - prints the sign of a number.
+* @n : type Integer.
+* Return: returns 1 and prints + if n is greater than zero.
+* Returns 0 and prints 0 if n is zero.
+* Returns -1 and prints - if n is less than zero.
+*/
 int print_sign(int n)
 {
 
