fac5eb5603fdc28f304176734fd807704e648a27
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-13 13:13:12 -0500

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*_isupper -  checks for uppercase character.
+* @c : int type Integer.
+* Return: 1 o 0;
+*/
+
+int _isupper(int c)
+{
+
+if (c <= 'Z')
+{
+return (1);
+
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+<strong>
+<h1>
+C - More functions, more nested loops
+</h1>
+</strong>
