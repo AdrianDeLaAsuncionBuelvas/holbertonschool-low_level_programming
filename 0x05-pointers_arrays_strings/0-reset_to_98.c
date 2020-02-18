802ad0b23f1c3b4b1c083c934222897949e65d2c
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-18 09:31:46 -0500

    a equal to b

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- *reset_to_98 - int as parameter and updates the value it points to to 98.
- *@n: int type Integer.
- *Return: void.
+ * reset_to_98 - int as parameter and updates the value it points to to 98.
+ * @n: int type Integer.
+ * Return: void.
  */
 void reset_to_98(int *n)
 {
