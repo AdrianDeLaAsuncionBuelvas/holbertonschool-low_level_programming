b72886bbc5e2206144f2287842a4ea5a6feec628
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-22 13:19:51 -0500

    encode a string using rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -2,28 +2,28 @@
 
 /**
  * rot13 - encodes a string using rot13
- * @s: char type Character
+ * @str: char type Character
  * Return: a
  */
 char *rot13(char *str)
 {
-	int i;
+int i;
 
-	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	char *a = str;
+char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+char *a = str;
 
-	while (*str)
-	{
-		for (i = 0; i <= 52; i++)
-		{
-			if (*str == rot13[i])
-			{
-				*str = ROT13[i];
-				break;
-			}
-		}
-		str++;
-	}
-	return (a);
+while (*str)
+{
+for (i = 0; i <= 52; i++)
+{
+if (*str == rot13[i])
+{
+*str = ROT13[i];
+break;
+}
+}
+str++;
+}
+return (a);
 }
