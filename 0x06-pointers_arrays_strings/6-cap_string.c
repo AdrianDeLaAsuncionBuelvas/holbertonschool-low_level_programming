4a74816a5bb28977a354ea7764332907775937cf
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-24 11:33:28 -0500

    Capitalizes all words of a String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,31 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
+ * cap_string - capitalizes all words of a string.
+ * @str: char type Character.
+ * Return: str.
  */
 
 char *cap_string(char *str)
 {
 
-	char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-	int a, b;
+char x[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int a, b;
 
+	for (a = 0; str[a] != '\0'; a++)
+	{
 
+		for (b = 0; x[b]; b++)
+		{
+			if (str[a - 1] == x[b])
+			{
+
+				if (str[a] >= 'a' && str[a] <= 'z')
+				{
+					str[a] = str[a] - 'a' + 'A';
+				}
+			}
+		}
+	}
+	return (str);
 }
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -7,23 +7,11 @@
  */
 int main(void)
 {
-    char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+       char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
     char *p;
 
-    p = rot13(s);
+    p = cap_string(s);
     printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    printf("------------------------------------\n");
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
-    printf("%s", s);
-    printf("------------------------------------\n");
-    p = rot13(s);
-    printf("%s", p);
-    printf("------------------------------------\n");
     printf("%s", s);
     return (0);
-
 }
