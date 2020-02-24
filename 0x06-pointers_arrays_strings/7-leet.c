1221bfab3c26433de9f8e96fa7673d8b4aefd57f
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-24 14:07:58 -0500

    enconde a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,14 +1,30 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
+ * leet - encodes a string into 1337
+ * @str: char type Character.
+ * Return: str.
  */
 
-char *leet(char *srt)
+char *leet(char *str)
 {
+	char min[] = {'a', 'e', 'o', 't', 'l'};
+	char may[] = {'A', 'E', 'O', 'T', 'L'};
+	char num[] = {'4', '3', '0', '7', '1'};
 
-	char *mos = "";
+	int i, a;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (a = 0; min[a] != '\0' && may[0]  != '\0'; a++)
+		{
+			if (str[i] == min[a] || str[i] == may[a])
+			{
+				str[i] = num[a];
+				break;
+			}
+		}
+	}
+	return (str);
+
 }
