3fee198613a8aa3808f72c2f2f471e7a98719ef9
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-24 19:22:02 -0500

    searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * -strpbrk - searches a string for any of a set of bytes.
+ * _strpbrk - searches a string for any of a set of bytes.
  * @s: Pointer char type Character.
  * @accept: Pointer char type Character
  * Return: accept.
@@ -22,17 +22,5 @@ char *_strpbrk(char *s, char *accept)
 			}
 		}
 	}
-	return ('\0');
-}
-
-
-int main(void)
-{
-    char *s = "hello, world";
-    char *f = "world";
-    char *t;
-
-    t = _strpbrk(s, f);
-    printf("%s\n", t);
-    return (0);
+	return (NULL);
 }
