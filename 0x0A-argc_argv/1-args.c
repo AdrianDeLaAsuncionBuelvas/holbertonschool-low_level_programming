94f3f243fa11fc4115df98070cb31290e28cd845
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-02-29 17:12:30 -0500

    prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main -  prints the number of arguments passed into it.
+ * @argc: type Integer
+ * @argv: Pointer type Integer
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+
+	}
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
