4fcc02513e4162d44a6a4032f4b67eeab08cbf59
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-03 19:34:03 -0500

    concatenates all the arguments of your program

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: int type Integer
+ * @av: Double Pointer
+ * Return: Pointer p
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, str, a;
+	char *p;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str++;
+		}
+	}
+
+	p = malloc(sizeof(char) * (str + ac + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			p[a] = av[i][j]; /*p = av[][]*/
+			a++; /*the variable starts at 0 and increases 1*/
+	}
+		p[a] = '\n';
+		a++;
+	}
+
+	return (p);
+}
