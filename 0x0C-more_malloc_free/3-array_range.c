27aa181dca62b5c992d205105f748cc63b4d1d5f
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-05 16:33:42 -0500

    concatenates two strings

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+}
