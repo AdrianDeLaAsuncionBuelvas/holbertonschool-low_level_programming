321a9404a0a0a32bce990be2993af915d87dc311
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-06 10:44:08 -0500

    reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,16 +18,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
 	free(ptr);
-	ptr = malloc(sizeof(char) * new_size);
 
+	ptr = malloc(sizeof(char) * new_size);
 	if (ptr == NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
+
 	return (ptr);
 }
