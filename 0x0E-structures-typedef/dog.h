9c21652fd320e5d0b95315c9dc46c342619752b8
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-09 17:03:59 -0500

    struct dog

--- 0x0E-structures-typedef/dog.h
+++ 0x0E-structures-typedef/dog.h
@@ -1,5 +1,15 @@
+#ifndef DJANGO_H
+#define DJANGO_H
 #include <stdio.h>
 
+/**
+ * struct dog - struct dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: Longer description
+ */
 
 struct dog
 {
@@ -7,3 +17,5 @@ struct dog
 	float age;
 	char *owner;
 };
+
+#endif
