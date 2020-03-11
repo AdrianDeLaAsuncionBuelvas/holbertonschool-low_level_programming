ee2628f0a76adb4f080c785a80e593c7a308e73d
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-11 17:05:42 -0500

    searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: Pointer type array
+ * @size: int type Integer
+ * @cmp: function Pointer type int
+ * Return: index
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if(size <= 0)
+		return (-1);
+
+	if ((*cmp) != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+		exit(0);
+	}
+
+	return (-1);
+}
