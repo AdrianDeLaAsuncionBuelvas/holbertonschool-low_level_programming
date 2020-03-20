c44c17fc7fedbd8560458f187cfd33329ce34e0a
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-19 22:14:24 -0500

    add the task avanced The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void Iam_a_Tortoise(void)__attribute__((constructor));
+
+/**
+ * Iam_a_Tortoise - pints a Strings
+ * The Hare and the Tortoise
+ */
+
+void Iam_a_Tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
