50c03006ab3fdcb535d6a5de0a97f8e7476c9ce3
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-23 17:38:19 -0500

    add task 4 test

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head != NULL)
+	{
+		node = head->next;
+		free(head);
+		head = node;
+	}
+}
