7e2359e077665679768e2558cd2d740d158fea83
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-23 13:12:28 -0500

    add task 2 return numbers of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - return elements of numbers
+ * @h: node of listint_t
+ * Return: numbers of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
