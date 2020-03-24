c626240f7351a7a457beaa858a936c92e91a6e0b
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-24 13:36:35 -0500

    add task 9 test 1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,8 +10,49 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t node;
-	listint_t temp;
+	listint_t *node;
+	listint_t *temp;
+	unsigned int count;
 
-	
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+
+	temp = NULL;
+	node->n = n;
+	node->next = NULL;
+
+
+	if (*head != NULL)
+	{
+
+		if (idx == 0)
+		{
+			node->next = *head;
+			*head = node;
+
+			return (node);
+		}
+
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			while (count == idx - 1)
+			{
+				node->next = temp->next;
+				temp->next = node;
+
+				return (node);
+			}
+			count++;
+			temp = temp->next;
+		}
+
+	}
+	else
+		return (NULL);
+
+	free(node);
+	return (*head);
 }
