92d4c2f5241d6c9985359a02caee6e7f1aa48caa
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-24 11:09:40 -0500

    add task 7 function returns the nth node of a listint_t

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -28,5 +28,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	else
 		return (NULL);
 
-	return (node);
+	return (head);
 }
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t node;
+	listint_t temp;
+
+	
+}
