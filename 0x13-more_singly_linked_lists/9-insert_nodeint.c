6dacd9e5024d152600164dd6f8f2bb31540135df
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-24 13:48:53 -0500

    add task 9 test 4

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,11 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (node == NULL)
 		return (NULL);
 
-	temp = NULL;
 	node->n = n;
-	node->next = NULL;
-
-
 	if (*head != NULL)
 	{
 
