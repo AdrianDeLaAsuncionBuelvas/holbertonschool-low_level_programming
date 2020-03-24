f538c5552b1bdbbc3a99899a2cac5d495a95a782
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-24 13:52:01 -0500

    add task 9 test 6

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = *head;
 		while (temp)
 		{
-			while (count == idx - 1)
+			if (count == idx - 1)
 			{
 				node->next = temp->next;
 				temp->next = node;
