e9f3aaa9ff8db0dd9779549d123bc74ff61768da
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-24 13:57:50 -0500

    add task 9 inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- *
- *
- *
- *
- *
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: Pointer to Pointer of head structure
+ * @idx: Index of the listint_t
+ * @n: Numbers that inserts at given position
+ * Return: NULL or New Node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
