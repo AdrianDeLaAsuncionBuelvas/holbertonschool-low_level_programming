1a0f7e9d9c8d94768f16977fa7437a86ede51ca1
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-24 22:41:07 -0500

    add task advanced 2 prints a listint test 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,26 +11,27 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *node;
 	const listint_t *temp;
 	int count = 0;
 
-	node = head;
+	temp = head;
 	if (head == NULL)
 		exit(98);
 
-	while (node != NULL)
+	while (temp != NULL)
 	{
-		temp = node;
-		node = node->next;
-		count++;
-
 		printf("[%p] %d\n", (void *)temp, temp->n);
 
-		if (temp < node)
+		if (temp <= temp->next)
 		{
-			printf("-> [%p] %d\n", (void *)node, node->n);
+			printf("-> [%p] %d\n", (void *)temp->next, temp->next->n);
+			count++;
 			break;
+		}
+		else
+		{
+			temp = temp->next;
+			count++;
 		}
 	}
 
