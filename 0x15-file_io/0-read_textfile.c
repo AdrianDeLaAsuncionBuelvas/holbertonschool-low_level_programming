de5ac8443f6dba28b73f44de02f89f6908af9b4c
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-31 15:27:15 -0500

    add task 0 reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: File Name
+ * @letters: size it will contain
+ * Return: actual number of letters
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -13,13 +13,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fread, fwrite, fd;
 
 	if (filename == NULL)
-		return(0);
+		return (0);
 
 	fd = open(filename, O_RDONLY, 0600);
 	if (fd == -1)
 		return (free(buffer), 0);
 
-	buffer = (char*) malloc(sizeof(char) * (letters + 1));
+/* Allocate a string that can hold it all*/
+	buffer = (char *) malloc(sizeof(char) * (letters + 1));
 	if (!buffer)
 		return (0);
 
@@ -28,14 +29,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buffer[letters] = '\0';
-
+	/* File descriptor STDOUT_FILENO */
 	fwrite = write(STDOUT_FILENO, buffer, fread);
 	if (fwrite == -1)
 		return (0);
-
+	/* Something went wrong, free the buffer*/
+	/* The buffer to NULL*/
 	free(buffer);
 	buffer = NULL;
 
+	/* Always close the file*/
 	close(fd);
 
 	return (fwrite);
