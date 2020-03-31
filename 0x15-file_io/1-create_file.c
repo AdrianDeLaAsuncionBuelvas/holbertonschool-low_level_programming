546d3556c735d091c5f5c1f30f057f5bf387010d
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-31 18:59:53 -0500

    add task 1 creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[size] != '\0')
+		while (text_content[size])
 			size++;
 
 	fwrite = write(fd, text_content, size);
