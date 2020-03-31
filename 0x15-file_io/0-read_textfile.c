bf61b2b8d34a8354015e05995e3bf1ced5d4dbc4
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-31 15:07:33 -0500

    add task 0 test1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,1 +1,42 @@
 #include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer = NULL;
+	int fread, fwrite, fd;
+
+	if (filename == NULL)
+		return(0);
+
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	buffer = (char*) malloc(sizeof(char) * (letters + 1));
+
+	fread = read(fd, buffer, letters);
+	if (fread == -1)
+		return (0);
+
+	buffer[letters] = '\0';
+
+	fwrite = write(STDOUT_FILENO, buffer, fread);
+
+	if (fwrite == -1)
+		return (0);
+
+	free(buffer);
+	buffer = NULL;
+
+	close(fd);
+
+	return (fwrite);
+}
