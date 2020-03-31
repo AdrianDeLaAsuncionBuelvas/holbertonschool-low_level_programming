47d6774c7e84f2addbbed787a4663c4d79ac1285
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-03-31 17:56:46 -0500

    add task 1 test 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,9 @@ int create_file(const char *filename, char *text_content)
 	ssize_t size = 0;
 	int fd, fwrite;
 
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_RDWR | O_CREAT | O_RDONLY, 0600);
 	if (fd == -1)
 		return (-1);
@@ -25,9 +28,11 @@ int create_file(const char *filename, char *text_content)
 	fwrite = write(fd, text_content, size);
 	if (fwrite == -1)
 		return(-1);
+
+	return(1);
 	}
 
 	close(fd);
-	return (1);
+	return (-1);
 
 }
