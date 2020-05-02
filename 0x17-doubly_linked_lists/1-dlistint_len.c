3551e8a1a7a261e967f3bf6ed24f35fe4a711a6f
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-05-02 16:30:34 -0500

    add first Task print elements numbers

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print datas and  return elements numbers
+ * @h: head that containt datas from node
+ * Return: number of elements
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *node = h;
+	size_t count = 0;
+
+	if (node == NULL)
+		return (0);
+	while (node)
+	{
+		node = node->next;
+		count++;
+	}
+	return (count);
+}
