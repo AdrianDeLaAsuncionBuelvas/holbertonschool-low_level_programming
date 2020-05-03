5ab039f96b775f482d11be744a75f377733022d2
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-05-02 21:14:31 -0500

    add Seventh Task inserts a new node at a given position. test 1

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,12 +29,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (count == idx)
 		return (add_dnodeint_end(h, n));
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
 	node = *h;
-	while (node && idx)
+	while (idx)
 		--idx, node = node->next;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
 	new->n = n;
 	new->next = node->next;
