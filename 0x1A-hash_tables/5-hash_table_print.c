4b8de0edd9127e560071648c99bcd7cc0f93c766
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-05-29 16:49:48 -0500

    add 5 Task function that prints a hash table.

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table.
+ *
+ * @ht: is the hash table.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp = NULL;
+	unsigned long int i = 0;
+	int end = 0;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			temp = ht->array[i];
+			while (temp)
+			{
+				if (end == 0)
+				{
+					printf("%s", "");
+				}
+				else
+					printf(", ");
+				printf("'%s' : '%s'", temp->key, temp->value);
+				temp = temp->next;
+				end++;
+			}
+		}
+	}
+	printf("}\n");
+}
