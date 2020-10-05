bf6b81cfffbe97ec0149b4237c9eae573ed068be
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-10-05 17:28:54 -0500

    check task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,14 +14,14 @@ char *cap_string(char *s)
 	{
 		if (s[0] >= 97 && s[0] <= 122)
 		{
-			s[0] = s[0] - 32;
+			s[0] = s[0] - 'a' + 'A';
 		}
 		for (j = 0; token[j]; j++) {
 			if (s[a - 1] == token[j])
 			{
 				if (s[a] >= 'a' && s[a] <= 'z')
 				{
-					s[a] = s[a] - 32;
+					s[a] = s[a] - 'a' + 'A';
 				}
 			}
 		}
